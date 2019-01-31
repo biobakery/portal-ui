d1679cb2dd495c0259b930dc3d7cc0f7eb8fb58e
ljmciver <lauren.j.mciver@gmail.com>
2019-01-31 00:13:01 +0000

    Remove project link and extra columns from projects table on projects page

--- src/packages/@ncigdc/modern_components/ProjectsTable/ProjectsTable.model.js
+++ src/packages/@ncigdc/modern_components/ProjectsTable/ProjectsTable.model.js
@@ -4,7 +4,6 @@ import {
   RepositoryCasesLink,
   RepositoryFilesLink,
 } from '@ncigdc/components/Links/RepositoryLink';
-import ProjectLink from '@ncigdc/components/Links/ProjectLink';
 import { Th, Td, ThNum, TdNum } from '@ncigdc/uikit/Table';
 import { makeFilter } from '@ncigdc/utils/filters';
 import formatFileSize from '@ncigdc/utils/formatFileSize';
@@ -14,40 +13,6 @@ import CollapsibleList from '@ncigdc/uikit/CollapsibleList';
 
 type TLinkProps = { node: Object, fields?: Array<Object>, children?: mixed };
 type TLink = (props: TLinkProps) => any;
-
-const dataCategoryColumns = createDataCategoryColumns({
-  title: 'Available Cases per Data Category',
-  countKey: 'case_count',
-  Link: RepositoryCasesLink,
-  getCellLinkFilters: node => [
-    {
-      field: 'cases.project.project_id',
-      value: node.project_id,
-    },
-  ],
-  getTotalLinkFilters: hits => [
-    {
-      field: 'cases.project.project_id',
-      value: hits.edges.map(({ node: p }) => p.project_id),
-    },
-  ],
-});
-
-const CasesLink: TLink = ({ node, fields = [], children }) =>
-  children === '0' ? (
-    <span>0</span>
-  ) : (
-    <RepositoryCasesLink
-      query={{
-        filters: makeFilter([
-          { field: 'cases.project.project_id', value: [node.project_id] },
-          ...fields,
-        ]),
-      }}
-    >
-      {children}
-    </RepositoryCasesLink>
-  );
 
 const getProjectIdFilter = projects =>
   makeFilter([
@@ -66,7 +31,7 @@ const projectsTableModel = [
     th: () => <Th rowSpan="2">Project</Th>,
     td: ({ node }) => (
       <Td>
-        <ProjectLink uuid={node.project_id}>{node.project_id}</ProjectLink>
+        {node.project_id}
       </Td>
     ),
   },
@@ -144,56 +109,14 @@ const projectsTableModel = [
     th: () => <ThNum rowSpan="2">Cases</ThNum>,
     td: ({ node }) => (
       <TdNum>
-        <CasesLink node={node}>
-          {node.summary.case_count.toLocaleString()}
-        </CasesLink>
+        {node.summary.case_count.toLocaleString()}
       </TdNum>
     ),
     total: withRouter(({ hits, query }) => (
       <TdNum>
-        <RepositoryCasesLink
-          query={{
-            filters: query.filters ? getProjectIdFilter(hits) : null,
-          }}
-        >
           {hits.edges
             .reduce((acc, val) => acc + val.node.summary.case_count, 0)
             .toLocaleString()}
-        </RepositoryCasesLink>
-      </TdNum>
-    )),
-  },
-  ...dataCategoryColumns,
-  {
-    name: 'Files',
-    id: 'summary.file_count',
-    sortable: true,
-    downloadable: true,
-    th: () => <ThNum rowSpan="2">Files</ThNum>,
-    td: ({ node }) => (
-      <TdNum>
-        <RepositoryFilesLink
-          query={{
-            filters: makeFilter([
-              { field: 'cases.project.project_id', value: node.project_id },
-            ]),
-          }}
-        >
-          {node.summary.file_count.toLocaleString()}
-        </RepositoryFilesLink>
-      </TdNum>
-    ),
-    total: withRouter(({ hits, query }) => (
-      <TdNum>
-        <RepositoryFilesLink
-          query={{
-            filters: query.filters ? getProjectIdFilter(hits) : null,
-          }}
-        >
-          {hits.edges
-            .reduce((acc, val) => acc + val.node.summary.file_count, 0)
-            .toLocaleString()}
-        </RepositoryFilesLink>
       </TdNum>
     )),
   },
