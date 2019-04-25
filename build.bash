6512a312c9082a12ea33fd867a435eec1c8c3af3
ljmciver <lauren.j.mciver@gmail.com>
2019-04-25 00:39:43 +0000

    Add npm build to build script

--- build.bash
+++ build.bash
@@ -2,6 +2,7 @@
 # to remove existing container, build and start new
 
 git pull
+npm run-script build
 sudo docker build -t portal-ui .
 sudo docker stop portal-ui && sudo docker rm portal-ui
 sudo docker run -d --network=host --name portal-ui portal-ui
