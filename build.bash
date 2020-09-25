0b3bb391e5852cc3cf084e7bc6f3c27c9513ec9a
ljmciver <lauren.j.mciver@gmail.com>
2020-09-25 20:35:54 +0000

    Update nginx and cert locations to defaults for certbot

--- build.bash
+++ build.bash
@@ -2,9 +2,9 @@
 # to remove existing container, build and start new
 
 git pull
-rm -rf /home/hutlab_public/portal-ui/live
+sudo rm -rf /usr/share/nginx/html
 npm run-script build
 sudo docker build -t portal-ui .
 sudo docker stop portal-ui && sudo docker rm portal-ui
-cp -r /home/hutlab_public/portal-ui/build /home/hutlab_public/portal-ui/live
-sudo docker run -d --network=host -v /home/hutlab_public/portal-ui/live/:/usr/share/nginx/html/ -v /home/hutlab_public/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /home/hutlab_public/letsencrypt/config:/etc/letsencrypt -v /home/hutlab_public/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
+sudo cp -r /home/hutlab_public/portal-ui/build /usr/share/nginx/html
+sudo docker run -d --network=host -v /usr/share/nginx/html:/usr/share/nginx/html -v /home/hutlab_public/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /etc/letsencrypt:/etc/letsencrypt -v /home/hutlab_public/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
