4f51d215e6c4371255f6ed4b7122d396fe86359b
ljmciver <lauren.j.mciver@gmail.com>
2020-11-23 22:06:46 +0000

    Make bash build paths generic to home dir

--- build.bash
+++ build.bash
@@ -6,5 +6,5 @@ sudo rm -rf /usr/share/nginx/html
 npm run-script build
 sudo docker build -t portal-ui .
 sudo docker stop portal-ui && sudo docker rm portal-ui
-sudo cp -r /home/hutlab_public/portal-ui/build /usr/share/nginx/html
-sudo docker run -d --network=host -v /usr/share/nginx/html:/usr/share/nginx/html -v /home/hutlab_public/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /etc/letsencrypt:/etc/letsencrypt -v /home/hutlab_public/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
+sudo cp -r ~/portal-ui/build /usr/share/nginx/html
+sudo docker run -d --network=host -v /usr/share/nginx/html:/usr/share/nginx/html -v ~/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /etc/letsencrypt:/etc/letsencrypt -v ~/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
