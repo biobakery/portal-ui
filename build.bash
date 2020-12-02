02ebc5f5e12fdf00100c31612dada4a0aace639b
ljmciver <lauren.j.mciver@gmail.com>
2020-12-02 21:31:19 +0000

    Replace in build with HOME to resolve docker tilde issues

--- build.bash
+++ build.bash
@@ -6,5 +6,5 @@ sudo rm -rf /usr/share/nginx/html
 npm run-script build
 sudo docker build -t portal-ui .
 sudo docker stop portal-ui && sudo docker rm portal-ui
-sudo cp -r ~/portal-ui/build /usr/share/nginx/html
-sudo docker run -d --network=host -v /usr/share/nginx/html:/usr/share/nginx/html -v ~/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /etc/letsencrypt:/etc/letsencrypt -v ~/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
+sudo cp -r $HOME/portal-ui/build /usr/share/nginx/html
+sudo docker run -d --network=host -v /usr/share/nginx/html:/usr/share/nginx/html -v $HOME/portal-ui/infra/nginx.conf:/etc/nginx/conf.d/nginx.conf -v /etc/letsencrypt:/etc/letsencrypt -v $HOME/ssl_cert_dhparam:/etc/ssl_cert --name portal-ui portal-ui
