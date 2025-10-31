e33ed4b41c82b2576378ce458b3ac67484e3975d
sagun <sagun_90@hotmail.com>
2025-10-31 04:54:24 -0400

    Kidney page changes

--- src/packages/@ncigdc/routes/KidneyRoute.js
+++ src/packages/@ncigdc/routes/KidneyRoute.js
@@ -48,7 +48,7 @@ const GradientContainer = styled(Row, {
 const containerStyle = {
   flex: 1,
   padding: '3rem',
-  height: '50rem',
+  height: '55rem',
   position: 'relative',
 };
 
@@ -85,7 +85,34 @@ Kidney stones: Stool sequencing data collected from women 55-75 years old and en
 
 <br/>
 <br/>
-Visit the repository page and search for "Kidney" to list <a href="https://biom-mass.org/repository?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.project.program.name%22%2C%22value%22%3A%5B%22Kidney%22%5D%7D%7D%5D%7D">all of the files for the Kidney project</a>. 
+<div style={{ textAlign: 'center', marginTop: '1.5rem' }}>
+  <p
+    style={{
+      fontWeight: 600,
+      fontSize: '1.1rem',
+      marginBottom: '0.75rem',
+    }}
+  >
+    <a
+      href="https://biom-mass.org/repository?facetTab=files&filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.project.program.name%22%2C%22value%22%3A%5B%22Kidney%22%5D%7D%7D%2C%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22files.data_merged%22%2C%22value%22%3A%5B%22yes%22%5D%7D%7D%5D%7D"
+      target="_blank"
+      style={{
+        display: 'inline-block',
+        backgroundColor: '#d32f2f',
+        color: '#ffffff',
+        padding: '10px 20px',
+        borderRadius: '6px',
+        textDecoration: 'none',
+        transition: 'background-color 0.3s ease',
+      }}
+      onMouseEnter={(e) => (e.target.style.backgroundColor = '#b71c1c')}
+      onMouseLeave={(e) => (e.target.style.backgroundColor = '#d32f2f')}
+    >
+      Access or download all data for Kidney Project
+    </a>
+  </p>
+</div>
+
 <br/>
 <br/>
 SRA bioproject: PRJNA1027357 (embargoed until 10/2025 or publication)
